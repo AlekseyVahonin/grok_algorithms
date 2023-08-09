f715029fd68b71709be0b18690056c2f7e6a3cef
Aleksey <aleksey.vahoninnd@gmail.com>
2023-08-09 14:44:55 +0300

    fact-recursion

---
+++ recursion/Program.cs
@@ -0,0 +1,17 @@
+﻿// Вычислить фаториал числа, рекурсивно.
+
+int Fact(int num)
+{
+    checked
+    {
+        if (num == 1) return num;
+        else return num *= Fact(num - 1);
+    }
+
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = Fact(number);
+Console.WriteLine(result);
